eecc5db5cf4d879721ede2ac606622d7f89338bf
rangasanju <rangasanju@gmail.com>
2023-02-12 18:01:07 +1100

    Initial Commit

---
+++ src/main/java/com/infyniteloop/soberail/SoberailApplication.java
@@ -0,0 +1,13 @@
+package com.infyniteloop.soberail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoberailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoberailApplication.class, args);
+	}
+
+}
