0c95a538e0a6e114860476f7ec0f4adeac382851
rangasanju <rangasanju@gmail.com>
2023-02-13 12:50:13 +1100

    Implemented Global Exception Handling

--- src/main/java/com/infyniteloop/soberail/response/BreathResultResponseDto.java
+++ src/main/java/com/infyniteloop/soberail/response/BreathResultResponseDto.java
@@ -3,7 +3,6 @@ package com.infyniteloop.soberail.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.jpa.repository.Meta;
 
 @Data
 @NoArgsConstructor
